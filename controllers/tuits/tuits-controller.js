9a3c42324e8e7b23efd8693b9d4efaa2973c4603
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2022-11-30 15:41:34 -0500

    render deploy

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,37 +1,53 @@
 import * as tuitsDao from './tuits-dao.js'
 
 const createTuit = async (req, res) => {
-    const newTuit = req.body
-    newTuit.likes = 0
-    newTuit.dislikes = 0
-    newTuit.replies = 0
-    newTuit.retuits = 0
-    newTuit.liked = false
-    newTuit.username = "NASA"
-    newTuit.handle = "@nasa"
-    newTuit.image = "../images/nasa-logo2.png"
-    newTuit.time = "Just now"
-    newTuit.topic = "Space"
-    const insertedTuit = await tuitsDao.createTuit(newTuit);
-    res.json(insertedTuit)
+    try {
+        const newTuit = req.body
+        newTuit.likes = 0
+        newTuit.dislikes = 0
+        newTuit.replies = 0
+        newTuit.retuits = 0
+        newTuit.liked = false
+        newTuit.username = "NASA"
+        newTuit.handle = "@nasa"
+        newTuit.image = "../images/nasa-logo2.png"
+        newTuit.time = "Just now"
+        newTuit.topic = "Space"
+        const insertedTuit = await tuitsDao.createTuit(newTuit);
+        res.json(insertedTuit)
+    } catch (err) {
+        res.sendStatus(503)
+    }
 }
 
 const findTuits  = async (req, res) => {
-    const tuits = await tuitsDao.findTuits()
-    res.json(tuits)
+    try {
+        const tuits = await tuitsDao.findTuits()
+        res.json(tuits)
+    } catch (err) {
+        res.sendStatus(503)
+    }
 }
 
 const updateTuit = async (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const updates = req.body;
-    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
-    res.json(status);
+    try {
+        const tuitdIdToUpdate = req.params.tid;
+        const updates = req.body;
+        const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+        res.json(status);
+    } catch (err) {
+        res.sendStatus(503)
+    }
 }
 
 const deleteTuit = async (req, res) => {
-    const tuitdIdToDelete = req.params.tid
-    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-    res.json(status)
+    try {
+        const tuitdIdToDelete = req.params.tid
+        const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+        res.json(status)
+    } catch (err) {
+        res.sendStatus(503)
+    }
 }
 
 
