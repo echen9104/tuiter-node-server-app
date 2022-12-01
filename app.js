345ac4e8617d8381b366d0ce2861da4e8dbcf29f
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2022-12-01 00:00:15 -0500

    render t7

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
 import mongoose from "mongoose";
 
+/*
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
    || 'mongodb://localhost:27017/tuiter'
+console.log(CONNECTION_STRING)
+ */
+const CONNECTION_STRING = 'mongodb+srv://echen9104:eric@cluster0.ibvarg8.mongodb.net/tuiter?retryWrites=true&w=majority'
 console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 const app = express()
